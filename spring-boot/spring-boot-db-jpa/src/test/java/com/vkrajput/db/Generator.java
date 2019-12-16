0fa1a7f0b9783836fafea773b59dc825255272f0
Vijay Kumar Rajput <vijaykrajpoot@gmail.com>
2019-12-15 17:45:58 -0800

    Added Testcases and configured H2

--- spring-boot/spring-boot-db-jpa/src/test/java/com/vkrajput/db/Generator.java
+++ spring-boot/spring-boot-db-jpa/src/test/java/com/vkrajput/db/Generator.java
@@ -4,17 +4,34 @@ import com.vkrajput.db.model.EmployeeDO;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Generator {
 
     public static EmployeeDO generateEmployee(){
-
         EmployeeDO employeeDO=new EmployeeDO();
         employeeDO.setAge(RandomUtils.nextInt(18,60));
         employeeDO.setFirstName(RandomStringUtils.randomAlphabetic(10,15).toUpperCase());
         employeeDO.setLastName(RandomStringUtils.randomAlphabetic(10,15).toUpperCase());
-
         return  employeeDO;
+    }
+
+
+    public static List<EmployeeDO>  generateEmployees(int noofEmployees){
+
+        List<EmployeeDO> employeeDOList=new LinkedList<>();
+        for(int i=0;i<noofEmployees;i++){
+            EmployeeDO employeeDO=new EmployeeDO();
+            employeeDO.setAge(RandomUtils.nextInt(18,60));
+            employeeDO.setFirstName(RandomStringUtils.randomAlphabetic(10,15).toUpperCase());
+            employeeDO.setLastName(RandomStringUtils.randomAlphabetic(10,15).toUpperCase());
+            employeeDOList.add(employeeDO);
+        }
+
+        return  employeeDOList;
 
     }
+
 
 }
--- spring-boot/spring-boot-db-jpa/src/test/java/com/vkrajput/db/H2DataSourceConfig.java
+++ spring-boot/spring-boot-db-jpa/src/test/java/com/vkrajput/db/H2DataSourceConfig.java
@@ -2,41 +2,38 @@ package com.vkrajput.db;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @Getter
 @Setter
+@EnableTransactionManagement
 public class H2DataSourceConfig {
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.hikari.username}")
-    private String username;
-
-    @Value("${spring.datasource.hikari.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder
-                .create()
-                .driverClassName(driverClassName)
-                .url(url)
-                .username(username)
-                .password(password)
-                .build();
-    }
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//
+//    @Value("${spring.datasource.hikari.username}")
+//    private String username;
+//
+//    @Value("${spring.datasource.hikari.password}")
+//    private String password;
+//
+//    @Value("${spring.datasource.driver-class-name}")
+//    private String driverClassName;
+//
+//    @Bean(name = "H2DataSource")
+//    public DataSource dataSource() {
+//        return DataSourceBuilder
+//                .create()
+//                .driverClassName(driverClassName)
+//                .url(url)
+//                .username(username)
+//                .password(password)
+//                .build();
+//    }
 
 }
